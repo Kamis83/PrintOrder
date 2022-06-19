96d6c925c7e550cecf88d435d0774227d196977f
Kamis83 <bajerka@gmail.com>
2022-06-19 09:23:27 +0200

    Commit 2 ( with tests for optimize)

--- src/main/java/com/optimization/Optimization.java
+++ src/main/java/com/optimization/Optimization.java
@@ -6,8 +6,6 @@ public class Optimization {
     public List<Layout> optimize(List<SheetOrder> order) {
 
 
-
-
         switch (order.size()) {
 
 
@@ -28,12 +26,13 @@ public class Optimization {
                 split = (double) order.get(0).quantityOfOrder() / (double) order.get(1).quantityOfOrder();
                 double split2 = (double) order.get(1).quantityOfOrder() / (double) order.get(2).quantityOfOrder();
                 double split3 = (double) order.get(0).quantityOfOrder() / (double) order.get(2).quantityOfOrder();
+
                 if (split == 1 && split2 != 0.5 || split2 == 1 && split != 2 || split3 == 1 && split != 0.5) {
 
                     return List.of(new Layout("2/4 + 2/4", 1, "side overturn"),
                             new Layout("4/4", 1, "side overturn"));
 
-                } else if (split == 1 && split2 == 0.5 || split2 == 1 && split == 2 || split3 == 1 && split == 0.5) {
+                } else if (split == 1 || split2 == 1 || split3 == 1) {
                     return List.of(new Layout("1/4 + 1/4 + 2/4", 2, "without overturn"));
 
                 } else {
@@ -56,11 +55,33 @@ public class Optimization {
                     return List.of(new Layout("2/4 + 2/4", 1, "side overturn"),
                             new Layout("2/4 + 2/4", 1, "side overturn"));
 
+                } else if (split == 1 && (split6 == 3.00 || split6 == 1.00 / 3.00) ||
+                        split6 == 1 && (split == 3.00 || split == 1.00 / 3.00) ||
+                        split2 == 1 && (split4 == 3.00 || split4 == 1.00 / 3.00) ||
+                        split4 == 1 && (split2 == 3.00 || split2 == 1.00 / 3.00) ||
+                        split3 == 1 && (split5 == 3.00 || split5 == 1.00 / 3.00) ||
+                        split5 == 1 && (split3 == 3.00 || split3 == 1.00 / 3.00)) {
+                    return List.of(new Layout("2/4 + 2/4", 1, "side overturn"),
+                            new Layout("1/4 + 3/4", 2, "without overturn"));
+                } else if (split == 1 && (split3 == 0.5 || split4 == 0.5) ||
+                        split2 == 1 && (split == 2 || split5 == 0.5) ||
+                        split3 == 1 && (split == 0.5 || split4 == 0.5) ||
+                        split4 == 1 && (split == 0.5 || split3 == 0.5) ||
+                        split5 == 1 && (split == 2 || split6 == 2) ||
+                        split6 == 1 && (split3 == 2 || split4 == 6)) {
+                    return List.of(new Layout("1/4 + 1/4 + 2/4", 2, "without overturn"),
+                            new Layout("4/4", 1, "side overturn"));
+                } else {
+                    return List.of(new Layout("4/4", 1, "side overturn"),
+                            new Layout("4/4", 1, "side overturn"),
+                            new Layout("4/4", 1, "side overturn"),
+                            new Layout("4/4", 1, "side overturn"));
+
                 }
 
 
         }
-        return null;
+
     }
 }
 
--- src/test/java/com/optimization/OptimizationTest.java
+++ src/test/java/com/optimization/OptimizationTest.java
@@ -139,5 +139,54 @@ class OptimizationTest {
                 new Layout("2/4 + 2/4", 1, "side overturn"));
     }
 
+    @Test
+    void optimization9() {
+        List<SheetOrder> order = List.of(
+                new SheetOrder(1000, "A", "B"),
+                new SheetOrder(2000, "C", "D"),
+                new SheetOrder(3000, "E", "F"),
+                new SheetOrder(2000, "G", "H"));
+
+
+        List<Layout> actual = new Optimization().optimize(order);
+
+        assertThat(actual).containsExactlyInAnyOrder(
+                new Layout("2/4 + 2/4", 1, "side overturn"),
+                new Layout("1/4 + 3/4", 2, "without overturn"));
+    }
+
+    @Test
+    void optimization10() {
+        List<SheetOrder> order = List.of(
+                new SheetOrder(2000, "A", "B"),
+                new SheetOrder(2000, "C", "D"),
+                new SheetOrder(4000, "E", "F"),
+                new SheetOrder(2000, "G", "H"));
+
+
+        List<Layout> actual = new Optimization().optimize(order);
+
+        assertThat(actual).containsExactlyInAnyOrder(
+                new Layout("1/4 + 1/4 + 2/4", 2, "without overturn"),
+                new Layout("4/4", 1, "side overturn"));
+    }
+
+    @Test
+    void optimization11() {
+        List<SheetOrder> order = List.of(
+                new SheetOrder(1000, "A", "B"),
+                new SheetOrder(20000, "C", "D"),
+                new SheetOrder(33000, "E", "F"),
+                new SheetOrder(3000, "G", "H"));
+
+
+        List<Layout> actual = new Optimization().optimize(order);
+
+        assertThat(actual).containsExactlyInAnyOrder(
+                new Layout("4/4", 1, "side overturn"),
+                new Layout("4/4", 1, "side overturn"),
+                new Layout("4/4", 1, "side overturn"),
+                new Layout("4/4", 1, "side overturn"));
+    }
 
 }
